6407f10ecf9b2a6d85f1dd6a1586f398587e8363
Spencer Curran <s.curran@setonhill.edu>
2020-02-07 21:56:20 -0500

    Initial Commit

---
+++ Mad LIbs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mad LIbs
+//
+//  Created by Spencer Curran on 2/7/20.
+//  Copyright © 2020 Spencer Curran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

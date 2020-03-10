65572e922842c0b4de67f3e69bbd7b2722a26716
Spencer Curran <s.curran@setonhill.edu>
2020-03-10 11:02:44 -0400

    comment

--- Mad LIbs/ViewController.swift
+++ Mad LIbs/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var paragraphGenerator: String = "Me and my friends went to the store and bought some <nounEnter>. The <nounEnter> we bought is really <adjectiveEnter> and fun ! After this purchase we <adverbEnter> ran to McDonalds and got some <foodEnter> but they were fresh out ! So we went in to find the manager and <pastEnter> right up to him and <adverbEnter> punched him in the face and took our business elsewhere."
+    
+    
+    
+    
+    
+    @IBOutlet weak var nounEnter: UITextField!
+    @IBOutlet weak var adjectiveEnter: UITextField!
+    @IBOutlet weak var adverbEnter: UITextField!
+    @IBOutlet weak var foodEnter: UITextField!
+    @IBOutlet weak var pastEnter: UITextField!
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func madLib(_ sender: Any) {
+        textField.text = paragraphGenerator.replacingOccurrences(of: "<nounEnter>", with: nounEnter.text!)
+        textField.text = textField.text?.replacingOccurrences(of: "<adjectiveEnter>", with: adjectiveEnter.text!)
+        textField.text = textField.text?.replacingOccurrences(of: "<adverbEnter>", with: adverbEnter.text!)
+        textField.text = textField.text?.replacingOccurrences(of: "<foodEnter>", with: foodEnter.text!)
+        textField.text = textField.text?.replacingOccurrences(of: "<pastEnter>", with: pastEnter.text!)
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
